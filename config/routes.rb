238460f1ecc3ffd79445f7b5b2ea28b8537d4ded
keilsmit <keilsmit@yahoo.com>
2017-01-07 12:07:37 -0500

    root and default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'front_ends#static'
+
+  resources :front_ends
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+  get '/:indytiy(/:random)' => 'front_ends#static'
+
 end
