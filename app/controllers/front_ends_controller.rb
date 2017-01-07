5d56ca6b2bd82ea13999c0cd10ce280eacd7754b
keilsmit <keilsmit@yahoo.com>
2017-01-07 18:53:21 -0500

    changes

--- app/controllers/front_ends_controller.rb
+++ app/controllers/front_ends_controller.rb
@@ -8,8 +8,19 @@ class FrontEndsController < ApplicationController
   end
 
   def meet_up
-    # @meet_up = Typhoeus::Request.new(
-    # render json: @meet_up
+    request = Typhoeus::Request.get(
+      'https://api.meetup.com/find/groups',
+      method: :get,
+      params: {
+        :'photo-host' => 'public',
+        page: 10,
+        sig_id: 209483447,
+        category: 34,
+        sig: 'dbcf4e2b2b00d29075fec95599b6c76982658dae'
+        }
+      )
+    parsed = JSON.parse(request.body)
+    render json: parsed
   end
 
 
