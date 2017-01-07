71419ab3844b84cc732cf250f9cacc9fe1c6b248
keilsmit <keilsmit@yahoo.com>
2017-01-07 12:42:47 -0500

    views

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
   root 'front_ends#static'
 
   resources :front_ends
+
+
+  get '/:indytiy(/:random)' => 'front_ends#static'
+
+
+
+
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
 
-
-  get '/:indytiy(/:random)' => 'front_ends#static'
 
 end
