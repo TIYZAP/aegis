82183024a4f3d9b0203c54399fb5ec87ce7bb49e
TIYZAP <ironyardzap@gmail.com>
2017-01-08 13:08:42 -0500

    omniauth additions

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'front_ends#static'
-
-
   scope '/api' do
     resources :grad_chats
     resources :back_ends
-
     resources :front_ends
       get '/meetup' => 'front_ends#meet_up'
   end
-
+  devise_for :users, controllers: {
+  omniauth_callbacks: "users/omniauth_callbacks"
+}
 
   get '/:indytiy(/:random)' => 'front_ends#static'
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
 end
