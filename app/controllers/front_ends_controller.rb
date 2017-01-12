e37c5a2c1fe660a38ca46336fdb145dbfbb77be8
TIYZAP <ironyardzap@gmail.com>
2017-01-12 11:41:39 -0500

    Removed useless omninauth / devise junk

--- app/controllers/front_ends_controller.rb
+++ app/controllers/front_ends_controller.rb
@@ -7,21 +7,7 @@ class FrontEndsController < ApplicationController
   def index
   end
 
-  def meet_up
-    request = Typhoeus::Request.get(
-      'https://api.meetup.com/find/groups?',
-      method: :get,
-      params: {
-        :'photo-host' => 'public',
-        page: 20,
-        sig_id: 199727819,
-        category: 34,
-        sig: '5161f91e824504f8609ee47af53bfefb8fddb8a3'
-        }
-      )
-    parsed = JSON.parse(request.body)
-    render json: parsed
-  end
+
 
 
 end
